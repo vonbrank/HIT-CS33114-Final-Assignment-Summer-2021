5e61ddafe669979814b136253a70cb8872717f66
vonbrank <vonbrank@outlook.com>
2023-04-13 00:50:28 +0800

    build: 初始化后端项目

---
+++ server/src/main/kotlin/cn/edu/hit/coursety/CoursetyApplication.kt
@@ -0,0 +1,11 @@
+package cn.edu.hit.coursety
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CoursetyApplication
+
+fun main(args: Array<String>) {
+    runApplication<CoursetyApplication>(*args)
+}
---
+++ server/src/test/kotlin/cn/edu/hit/coursety/CoursetyApplicationTests.kt
@@ -0,0 +1,13 @@
+package cn.edu.hit.coursety
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoursetyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
